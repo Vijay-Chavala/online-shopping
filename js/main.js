01f0915385267328a6cb2a03f6100385a9a7d5cf
vijay <vijaychavala4455@gmail.com>
2021-09-06 22:52:19 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+
